9cfecca564dea7cd187eb24a144862f41c390a89
Mathew Denys <mathew@denys.nz>
2020-09-14 13:55:27 +1200

    Initial commit

---
+++ main.cpp
@@ -0,0 +1,19 @@
+#define _USE_MATH_DEFINES
+#include <cmath>    // M_Pi
+#include <iostream> // std::cout
+#include <complex>
+#include "Eigen/Dense"
+
+using complexnum = std::complex<double>; // easier to read, and if I need to change to e.g. long double through it will be easier
+
+int hallconductivity(int Nmax, double temp, int omegamax, double deltaomega, double zeroplus);
+
+int main()
+{
+    return 0;
+}
+
+int hallconductivity(int Nmax, double temp, int omegamax, double deltaomega, double zeroplus)
+{
+    return 0; // meaningless for now
+}
