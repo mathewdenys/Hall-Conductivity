a1fcc3231ad3dbf53c9859f2b9aa3bc0218b6ef7
Mathew Denys <mathew@denys.nz>
2020-09-18 17:26:01 +1200

    Got HallConductivity() working

--- main.cpp
+++ main.cpp
@@ -1,69 +1,68 @@
 #define _USE_MATH_DEFINES
-#include <cmath>    // M_Pi
-#include <complex>
 #include <iostream> // std::cout
+#include <cmath>    // M_Pi, exp
+#include <array>    // std::array
+#include <complex>
 #include "Eigen/Dense"
 
-using namespace std::literals::complex_literals;
-//using std::literals::complex_literals::operator""i; // Use literal suffix i (or if, il) to denote an imaginary number
-using complexd = std::complex<double>;              // easier to read, and if I need to change to e.g. long double through it will be easier
+using namespace std::literals::complex_literals; // Use literal suffix i (or if, il) to denote an imaginary number
+using complexd = std::complex<double>;
 using Eigen::Matrix2cd;
 using Eigen::Matrix4cd;
+using Eigen::Vector4cd;
 using std::cout;
 
 // Define parameters for the normal state
 const double t1  = 1.0;
 const double t2  = 0.8*t1;
 const double t3  = 0.1*t1;
-const double mu  = t1;
-const double soc = 0.25*t1;
+const double mu  = t1;          // chemical potential
+const double soc = 0.25*t1;     // spin-orbital coupling strength
 
 // Define interaction strengths
-const double lambda1 = -0.2;
-const double lambda2 = -0.265;
-const double lambda3 =  0.03;
+const double lambda1 = -0.2;    // interaction strength in the 01 channel
+const double lambda2 = -0.265;  // interaction strength in the 31 channel
+const double lambda3 =  0.03;   // inter-channel interaction strength
 
 // Define calculation parameters
-const int    Nk_gap   = 500;
-const int    Nk_hall  = 5000;
-const double T1       = 0.0001;
-const double T2       = 0.02;
-const int    NT       = 200;
-const double freq1    = 0.01;
-const double freq2    = 3.0;
-const int    Nfreq    = 300;
-const double zeroplus = 0.001;
+const int    Nk_gap    = 4;
+const int    Nk_hall   = 4;
+const double T1        = 0.0001;
+const double T2        = 0.02;
+const int    NT        = 200;
+const double deltaFreq = 0.01;
+const int    Nfreq     = 5;
+const double zeroPlus  = 0.001;
 
 struct DeltaWrap
 {
     complexd val[2];
 };
 
-struct HallParameters
+struct HallParameters // Parameters to be passed to the HallConductivity() function.
 {
-    int    Nmax       {0};	///< This is the number of lattice points per dimension
-	double temp       {0};	///< This is the temperature
-	int    omegaMax   {0};	///< This is the maximum frequency (of light) to consider
-	double deltaOmega {0};	///< This is the spacing between frequencies
-	double zeroplus   {0};	///< This is the numerical approximation of the positive infintessimal in the analytic continuation
+    int    Nk         {};	// The number of lattice points per dimension
+	double temp       {};	// The temperature
+	double deltaFreq  {};	// The spacing between frequencies
+    int    Nfreq      {};   // The number of frequencies to evaluate the Hall conductivity at
+	double zeroPlus   {};	// The numerical approximation of the positive infintessimal in the analytic continuation
 };
 
 class PauliMatrixGen
 {
-private:
-    Matrix2cd makeMatrix(const std::initializer_list<complexd>& args)
+private: // fixed this so it actually works. But can I make makeMatrix() neater?
+    Matrix2cd makeMatrix(const complexd& val00, const complexd& val01, const complexd& val10, const complexd& val11)
     {
         Matrix2cd matrix;
-        for (const complexd &arg : args)
-            matrix << arg;
+        matrix << val00, val01, val10, val11;
         return matrix;
     }
 
 public:
     Matrix2cd makePauli0() { return Matrix2cd::Identity(); }
-    Matrix2cd makePauli1() { return makeMatrix({0,  1,  1,  0}); }
-    Matrix2cd makePauli2() { return makeMatrix({0, -1i, 1i, 0}); }
-    Matrix2cd makePauli3() { return makeMatrix({1,  0,  0, -1}); }
+    Matrix2cd makePauli1() { return makeMatrix(0,  1,  1,  0); }
+    Matrix2cd makePauli2() { return makeMatrix(0, -1i, 1i, 0); }
+    Matrix2cd makePauli3() { return makeMatrix(1,  0,  0, -1); }
 };
 
 DeltaWrap DeltaConversion(const DeltaWrap& delta)
@@ -74,7 +73,7 @@ DeltaWrap DeltaConversion(const DeltaWrap& delta)
     return deltaConverted;
 }
 
-Matrix4cd kron(Matrix2cd A, Matrix2cd B) // kron is the Kronecker product between two matrices
+Matrix4cd kron(Matrix2cd A, Matrix2cd B) // The Kronecker product between two matrices
 {
     // implemented using formula from mathworld (https://mathworld.wolfram.com/KroneckerProduct.html)
     // using the notation used there (p,q,i,j,k,l)
@@ -98,31 +97,32 @@ Matrix4cd kron(Matrix2cd A, Matrix2cd B) // kron is the Kronecker product betwee
     return C;
 }
 
-int HallConductivity(const DeltaWrap& delta0optim, HallParameters& hall)
+void HallConductivity(std::array<complexd,Nfreq>& hallOut, const DeltaWrap& delta0optim, HallParameters& params)
 {
-    // TODO: INITIALIZE ARRAY TO STORE HALL CONDUCTIVITY
-
-    const int &Nmax = hall.Nmax; // do for other variables as well
+    // local variables for the HallParameters that are used more than once
+    const int    &N    = params.Nk;
+    const int    &temp = params.temp;
+    assert(N%2==0 && "N must be divisible by 2 in order to sum over partial Brillouin zone");
     
-    for (int nx=Nmax/2; nx<Nmax; ++nx)
-        for (int ny=Nmax/2; ny<Nmax; ++ny) {
-
+    // sum over only one eighth of the brillouin zone (note upper limit in each for loop)
+    for (int nx=N/2; nx<N; ++nx)
+        for (int ny=N/2; ny<=nx; ++ny) 
+        {
             // calculate momentum values
-            double kx {(2*nx-Nmax+1)*M_PI/Nmax};
-            double ky {(2*ny-Nmax+1)*M_PI/Nmax};
+            double kx {(2*nx-N+1)*M_PI/N};
+            double ky {(2*ny-N+1)*M_PI/N};
 
             // don't double count diagonals of the Brillouin zone
             double bzdiagonal = (nx==ny) ? 0.5 : 1.0;
 
             // make Pauli matrices
             PauliMatrixGen p;
-            Eigen::Matrix2cd pauli0 {p.makePauli0()};
-            Eigen::Matrix2cd pauli1 {p.makePauli1()};
-            Eigen::Matrix2cd pauli2 {p.makePauli2()};
-            Eigen::Matrix2cd pauli3 {p.makePauli3()};
+            Matrix2cd pauli0 {p.makePauli0()};
+            Matrix2cd pauli1 {p.makePauli1()};
+            Matrix2cd pauli2 {p.makePauli2()};
+            Matrix2cd pauli3 {p.makePauli3()};
 
             // construct normal state Hamiltonian
-            // these are all real-valued
             double h00 { -t1*(cos(kx)+cos(ky)) - mu};
             double h10 {2*t3*(sin(kx)*sin(ky))};
             double h30 { -t2*(cos(kx)-cos(ky))};
@@ -133,40 +133,73 @@ int HallConductivity(const DeltaWrap& delta0optim, HallParameters& hall)
             double v00y {t1*sin(ky)};
             double v10x {2*t3*cos(kx)*sin(ky)};
             double v10y {2*t3*sin(kx)*cos(ky)};
-            double v30x {t2*sin(kx)};
-            double v30y {t2*sin(ky)};
+            double v30x { t2*sin(kx)};
+            double v30y {-t2*sin(ky)};
             Matrix4cd Vx {v00x*kron(pauli0,pauli0) + v10x*kron(pauli0,pauli1) + v30x*kron(pauli0,pauli3)};
             Matrix4cd Vy {v00y*kron(pauli0,pauli0) + v10y*kron(pauli0,pauli1) + v30y*kron(pauli0,pauli3)};
-            
-            // construct pairing potential
-            DeltaWrap delta0optim_converted = DeltaConversion(delta0optim);
-            complexd d01 = delta0optim_converted.val[0]*(sin(kx)+1i*sin(ky));
-            complexd d31 = delta0optim_converted.val[1]*(sin(kx)-1i*sin(ky));
 
-            // explicitly construct & calculate evals of one sector of the BdG Hamiltonian
+            // construct pairing potential
+            complexd d01 = delta0optim.val[0]*(sin(kx)+1i*sin(ky));
+            complexd d31 = delta0optim.val[1]*(sin(kx)-1i*sin(ky));
+
+            // explicitly construct one sector of the BdG Hamiltonian
             Matrix4cd HBdg;
             HBdg << h00+h30, h10-1i*h23, d01+d31, 0,
                     h10+1i*h23, h00-h30, 0, d01-d31,
                     std::conj(d01)+std::conj(d31), 0, -h00-h30, -h10+1i*h23,
                     0, std::conj(d01)-std::conj(d31), -h10-1i*h23, -h00+h30;
+
+            // calculate eigenvalues and eigenvectors of BdG Hamiltonian
+            Eigen::ComplexEigenSolver<Matrix4cd> ces;
+            ces.compute(HBdg); // column k of .eigenvectors() corresponds to the kth .eigenvalue()
             
-            Eigen::ComplexEigenSolver<Matrix4cd> ces(HBdg,false);
-            Eigen::Vector4cd evals = ces.eigenvalues();
-            Eigen::Matrix4cd evecs = ces.eigenvectors();
+            std::cout<<1<<params.Nfreq<<'\n';
 
-            // TODO: PERFORM THE LOOP OVER MATSUBARA FREQUENCIES
+            for (int freqInd = 0; freqInd < params.Nfreq; freqInd++)
+            {
+                
+            std::cout<<2<<'\n';
+            
+                complexd omega = (freqInd+1)*params.deltaFreq + 1i*params.zeroPlus;
+                for (int i = 0; i < 4; i++)
+                {
+                    double    evali = real(ces.eigenvalues()[i]);
+                    Vector4cd eveci = ces.eigenvectors().col(i);
+                    for (int j = 0; j < 4; j++)
+                    {
+                        double    evalj = real(ces.eigenvalues()[j]);
+                        Vector4cd evecj = ces.eigenvectors().col(j);
+                        hallOut[freqInd] += 2i*imag(eveci.dot(Vx*evecj) * evecj.dot(Vy*eveci))
+                                             * (tanh(evali/(2*temp))-tanh(evalj/(2*temp)))
+                                            / ( real(omega)*(evali - evalj + omega) )
+                                            * bzdiagonal * 8.0 * 2.0 /(8i*pow(N,2)); // x8 for BZ summation; x2 for each sector; divide by prefactor
+                    }
+                }
+            }
         }
-
-    return 0; // meaningless for now
 }
 
 int main()
 {
-    DeltaWrap delta0optim; // in the future this will be determined (as a function of T), but for now I will assign it a value
+    // define pairing potential. In the future this will be calculated
+    DeltaWrap delta0optim;
     delta0optim.val[0] = 0.2;
     delta0optim.val[1] = 0.05;
 
-    // call Hall conductivity function
+    HallParameters params;
+    params.Nk = Nk_hall;
+    params.temp = T1;
+    params.deltaFreq = deltaFreq;
+    params.Nfreq = Nfreq;
+    params.zeroPlus = zeroPlus;
+
+    std::array<complexd,Nfreq> hall {}; // initialize array to store the Hall conductivity
+
+    HallConductivity(hall, delta0optim, params);
+
+    std::cout<<"\nhall after calling HallConductivity():\n"; // todo: rather than printing result, plot / save to file
+    for (int i = 0; i < Nfreq; i++)
+        std::cout << hall[i]<<std::endl;
 
     return 0;
 }
